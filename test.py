3ac4586c4cffb9de8d0c0094bbd828f582bf136d
Karan Kaushik <karankshk96@gmail.com>
2019-12-29 21:27:01 +0530

    update

--- test.py
+++ test.py
@@ -5,3 +5,18 @@ arr = 'karan k'
 for x in arr:
     print(kh.KEYS[x])
     pass
+
+
+while True:
+	key = input('-->')
+	try:
+		print(kh.KEYS[key.lower()])
+		pass
+	except:
+		for x in key:
+			print(kh.KEYS[x.lower()])
+			pass
+	finally:
+		pass
+	
+	pass
